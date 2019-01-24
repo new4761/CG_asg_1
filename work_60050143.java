403bee496671f4f46b69d389cfbd9ae002b54da8
new4761 <brid47@outlook.com>
2019-01-25 05:25:23 +0700

    up Date 5:25 143 1/25/62

--- work_60050143.java
+++ work_60050143.java
@@ -16,44 +16,136 @@ class work_60050143 extends CG_as_1
 	 	fillcolorX(g, set_CG(7), 54, 331, 29, 389);
 	 	fillcolorX(g, set_CG(7), 14, 435, 29, 389);
 	*/
+		draw_ice_color10(g);
+		draw_ice_color6(g);
+
+	}
+	private void draw_ice_color6(Graphics g){
 		//draw and fillcolor ice_color 6
-		int color_6 =6;
+		int color =6;
 		//top ice_color 6
-			fillcolorX(g, set_CG(color_6), 0, 80, 27, 35,0);
-			fillcolorX(g, set_CG(color_6), 48, 30, 27, 35,0);	
-			fillcolorX(g, set_CG(color_6), 48, 30, 62, 15,0);	
-			fillcolorX(g, set_CG(color_6), 95, 73, 62, 15,0);	
-			fillcolorX(g, set_CG(color_6), 95, 73, 134, 173,0);		
-			fillcolorX(g, set_CG(color_6), 163, 230, 134, 173,0);		
-			fillcolorX(g, set_CG(color_6), 163, 230, 134, 173,0);	
-			fillcolorX(g, set_CG(color_6), 163, 230, 143, 159,0);
-			fillcolorX(g, set_CG(color_6), 144, 117, 143, 159,0);
-			fillcolorX(g, set_CG(color_6), 144, 117, 152, 50,0);				
-			fillcolorX(g, set_CG(color_6), 160, 67, 152, 50,0);
-			fillcolorX(g, set_CG(color_6), 160, 67, 160, 79,0);		
-			fillcolorX(g, set_CG(color_6), 196, 161, 160, 79,0);
-			fillcolorX(g, set_CG(color_6), 196, 161, 224, 230,0);
-			fillcolorX(g, set_CG(color_6), 225, 127, 224, 230,0);
-			fillcolorX(g, set_CG(color_6), 225, 127, 239, 71,0);
-			fillcolorX(g, set_CG(color_6), 324,30, 239, 71,0);
-			fillcolorX(g, set_CG(color_6), 324,30, 331, 23,0);
-			fillcolorX(g, set_CG(color_6), 359,47, 331, 23,0);
-			fillcolorX(g, set_CG(color_6), 359,47, 370, 113,0);
-			fillcolorX(g, set_CG(color_6), 384,189, 370, 113,0);
-			fillcolorX(g, set_CG(color_6), 384,189, 392, 153,0);
-			fillcolorX(g, set_CG(color_6), 390,128, 392, 153,0);
-			fillcolorX(g, set_CG(color_6), 390,128, 403, 76,0);
-			fillcolorX(g, set_CG(color_6), 418,40, 403, 76,0);
-			fillcolorX(g, set_CG(color_6), 418,40, 455, 102,0);
-			fillcolorX(g, set_CG(color_6), 478,147, 455, 102,0);
-			fillcolorX(g, set_CG(color_6), 478,147, 507, 222,0);
-			fillcolorX(g, set_CG(color_6), 514,168, 507, 222,0);
-			fillcolorX(g, set_CG(color_6), 514,168, 532, 78,0);
-			fillcolorX(g, set_CG(color_6), 600,167, 532, 78,0);
+			fillcolorX(g, set_CG(color), 0, 80, 27, 35,0);
+			fillcolorX(g, set_CG(color), 48, 30, 27, 35,0);	
+			fillcolorX(g, set_CG(color), 48, 30, 62, 15,0);	
+			fillcolorX(g, set_CG(color), 95, 73, 62, 15,0);	
+			fillcolorX(g, set_CG(color), 95, 73, 134, 173,0);		
+			fillcolorX(g, set_CG(color), 163, 230, 134, 173,0);		
+			fillcolorX(g, set_CG(color), 163, 230, 134, 173,0);	
+			fillcolorX(g, set_CG(color), 163, 230, 143, 159,0);
+			fillcolorX(g, set_CG(color), 144, 117, 143, 159,0);
+			fillcolorX(g, set_CG(color), 144, 117, 152, 50,0);				
+			fillcolorX(g, set_CG(color), 160, 67, 152, 50,0);
+			fillcolorX(g, set_CG(color), 160, 67, 160, 79,0);		
+			fillcolorX(g, set_CG(color), 196, 161, 160, 79,0);
+			fillcolorX(g, set_CG(color), 196, 161, 224, 230,0);
+			fillcolorX(g, set_CG(color), 225, 127, 224, 230,0);
+			fillcolorX(g, set_CG(color), 225, 127, 239, 71,0);
+			fillcolorX(g, set_CG(color), 324,30, 239, 71,0);
+			fillcolorX(g, set_CG(color), 324,30, 331, 23,0);
+			fillcolorX(g, set_CG(color), 359,47, 331, 23,0);
+			fillcolorX(g, set_CG(color), 359,47, 370, 113,0);
+			fillcolorX(g, set_CG(color), 384,189, 370, 113,0);
+			fillcolorX(g, set_CG(color), 384,189, 392, 153,0);
+			fillcolorX(g, set_CG(color), 390,128, 392, 153,0);
+			fillcolorX(g, set_CG(color), 390,128, 403, 76,0);
+			fillcolorX(g, set_CG(color), 418,40, 403, 76,0);
+			fillcolorX(g, set_CG(color), 418,40, 455, 102,0);
+			fillcolorX(g, set_CG(color), 478,147, 455, 102,0);
+			fillcolorX(g, set_CG(color), 478,147, 507, 222,0);
+			fillcolorX(g, set_CG(color), 514,168, 507, 222,0);
+			fillcolorX(g, set_CG(color), 514,168, 532, 78,0);
+			fillcolorX(g, set_CG(color), 600,167, 532, 78,0);
 			
 			//bottom ice_color 6
-			fillcolorX(g, set_CG(color_6), 600,300, 532, 432,250,300);
-	}
+			fillcolorX(g, set_CG(color), 0,409, 34, 493,600);
+			fillcolorX(g, set_CG(color), 45,542, 34, 493,600);
+		
+			fillcolorX(g, set_CG(color), 70,493, 80,432,600);
+			fillcolorX(g, set_CG(color), 73,501, 45, 542,600);
+			fillcolorX(g, set_CG(color), 73,501, 82, 474,600);
+
+			fillcolorX(g, set_CG(color), 102,392, 82, 474,600);
+			fillcolorX(g, set_CG(color), 102,392, 112,310,600);
+			fillcolorX(g, set_CG(color), 102,392, 120, 376,600);
+			fillcolorX(g, set_CG(color), 125,360, 120, 376,600);
+			fillcolorX(g, set_CG(color), 125,360, 140, 345,400,350);
+			fillcolorX(g, set_CG(color), 140,345, 155, 330,357,310);
+			fillcolorX(g, set_CG(color), 120,450, 142,487,600);
+			fillcolorX(g, set_CG(color), 156,496, 142,487,600);
+			fillcolorX(g, set_CG(color), 156,496, 171,529,600);
+		
+			fillcolorX(g, set_CG(color), 171,529, 264, 600,600);
+			fillcolorX(g, set_CG(color), 295,570, 264, 600,600);
+			fillcolorX(g, set_CG(color), 295,570, 306, 551,600);
+			fillcolorX(g, set_CG(color), 319,595, 306, 551,600);
+			fillcolorX(g, set_CG(color), 405,600, 430, 564,600);
+			fillcolorX(g, set_CG(color), 467,522, 430, 564,600);
+			fillcolorX(g, set_CG(color), 467,522, 482, 483,600);
+			fillcolorX(g, set_CG(color), 492,466, 482, 483,600);
+			fillcolorX(g, set_CG(color), 492,466, 502, 429,600);
+			fillcolorX(g, set_CG(color), 512,399, 502, 429,600);
+			fillcolorX(g, set_CG(color), 512,399, 527, 468,600);
+			fillcolorX(g, set_CG(color), 534,507, 527, 468,600);
+			fillcolorX(g, set_CG(color), 550,536, 527, 468,600);
+			fillcolorX(g, set_CG(color), 550,536, 580,503,600);
+			fillcolorX(g, set_CG(color), 603,482, 580,503,600);
+
+
+		}
+		private void draw_ice_color10(Graphics g){
+			//draw and fillcolor ice_color 10
+			int color =10;
+			//top ice_color 10
+				fillcolorX(g, set_CG(color), 0, 93, 27, 43,0);
+				fillcolorX(g, set_CG(color), 48, 30+8, 27,43,0);	
+				fillcolorX(g, set_CG(color), 48, 30+8, 62, 15+8,0);	
+				fillcolorX(g, set_CG(color), 167,230+8, 143+3, 159+8,0);
+				fillcolorX(g, set_CG(color), 144, 117+8, 143, 159+8,0);
+				fillcolorX(g, set_CG(color), 144, 117+8, 152, 50+8,0);				
+				fillcolorX(g, set_CG(color), 160, 67+8, 152, 50+8,0);
+				fillcolorX(g, set_CG(color), 225+8, 127+8, 224, 230+8,0);
+				fillcolorX(g, set_CG(color), 225, 127+8, 239+8, 71+8,0);
+				fillcolorX(g, set_CG(color), 324+8,30+8, 239, 71+8,0);
+				fillcolorX(g, set_CG(color), 324,30+8, 331+8, 23+8,0);
+				fillcolorX(g, set_CG(color), 359+8,47+8, 331, 23+8,0);
+				fillcolorX(g, set_CG(color), 384,189+8, 392+8, 153+8,0);
+				fillcolorX(g, set_CG(color), 390+8,128+8, 392, 153+8,0);
+				fillcolorX(g, set_CG(color), 390,128+8, 403+8, 76+8,0);
+				fillcolorX(g, set_CG(color), 418+8,40+8, 403, 76+8,0);
+				fillcolorX(g, set_CG(color), 514+8,168+8, 507, 222+8,0);
+				fillcolorX(g, set_CG(color), 514,168+8, 532+8, 78+8,0);
+
+				
+				//bottom ice_color 10
+				fillcolorX(g, set_CG(color), 0,409, 34, 493,600);
+				fillcolorX(g, set_CG(color), 45,542, 34, 493,600);
+			
+				fillcolorX(g, set_CG(color), 73,493, 83,432,600);
+				fillcolorX(g, set_CG(color), 73,501, 45, 542,600);
+			
 	
+				fillcolorX(g, set_CG(color), 102+4,392+4, 82, 474+4,600);
+				fillcolorX(g, set_CG(color), 102,392+6, 112+2,310+6,600);
+				fillcolorX(g, set_CG(color), 102+8,392+8, 120+20, 376+8,600);
+				fillcolorX(g, set_CG(color), 125,360+8, 140+14, 345+8,400+8,350);
+				fillcolorX(g, set_CG(color), 140,345+10, 155, 330+10,357+10,310+10);
+				
+				fillcolorX(g, set_CG(color), 120+8,450+8, 142+30,487+8,600);
+				fillcolorX(g, set_CG(color), 156+20,496+8, 142+8,487+8,600);
+				fillcolorX(g, set_CG(color), 156+8,496+8, 171+20,529+8,600);
+			
+				fillcolorX(g, set_CG(color), 171+20,529+8, 264+8, 600+8,600);
+
+
+				fillcolorX(g, set_CG(color), 319+3,595, 306+3, 551,600);
+
+				fillcolorX(g, set_CG(color), 512+3,399+8, 527+8, 468+8,600);
+				fillcolorX(g, set_CG(color), 534+20,507+8, 527+8, 468+8,600);
+				fillcolorX(g, set_CG(color), 550+30,536+8, 527+10, 468+8,600);
+
+	
+	
+			}	
+			
 	
 }
