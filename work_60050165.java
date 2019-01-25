5360d3ea1e3f90796dc7c2a426207e6ee9798b5f
jump <jumpzaza1242@gmail.com>
2019-01-26 00:56:29 +0700

    background 1

--- work_60050165.java
+++ work_60050165.java
@@ -4,36 +4,158 @@ class work_60050165 extends CG_as_1{
 	work_60050165(Graphics g){		
 		plot(g,set_CG(9),0,0,600,600);//frist background
 	    background_layer1_color1(g);
-		background_layer1_color2(g);
-		background_layer1_color3(g);
-		icefloor_layer1_color12_8(g);
+		background_layer2_color2(g);
+		background_layer3_color3(g);
+		background_layer4_color12(g);
+		background_layer5_color8(g);
 		icepole_color9(g);
 	}//consutor
     private void background_layer1_color1(Graphics g){ //draw and fillcolor background_layer1
 		int color =1; //background_layer1_color1
-		   fillcolorX(g, set_CG(color), 166,120,400,120,400);
-           fillcolorX(g, set_CG(color), 400,120,600,200,400);
-		   fillcolorX(g, set_CG(color), 165,120,-200,300,400);
+		   fillcolorX(g, set_CG(color), 275,120,300,120,400);
+           
+		   fillcolorX(g, set_CG(color), 300,120,325,125,400);
+		   fillcolorX(g, set_CG(color), 325,125,350,130,400);
+		   fillcolorX(g, set_CG(color), 350,130,375,135,400);
+		   fillcolorX(g, set_CG(color), 375,135,400,140,400);
+		   fillcolorX(g, set_CG(color), 400,140,425,145,400);
+		   fillcolorX(g, set_CG(color), 425,145,450,150,400);
+		   fillcolorX(g, set_CG(color), 450,150,475,155,400);
+		   fillcolorX(g, set_CG(color), 475,155,500,160,400);
+		   fillcolorX(g, set_CG(color), 500,160,525,165,400);
+		   fillcolorX(g, set_CG(color), 525,165,550,170,400);
+		   fillcolorX(g, set_CG(color), 550,170,575,175,400);
+		   fillcolorX(g, set_CG(color), 575,175,600,180,400);
+           
+		   fillcolorX(g, set_CG(color), 275,120,250,125,400);
+		   fillcolorX(g, set_CG(color), 250,125,225,130,400);
+		   fillcolorX(g, set_CG(color), 225,130,200,135,400);
+		   fillcolorX(g, set_CG(color), 200,135,175,140,400);
+		   fillcolorX(g, set_CG(color), 175,140,150,145,400);
+		   fillcolorX(g, set_CG(color), 150,145,125,150,400);
+		   fillcolorX(g, set_CG(color), 125,150,100,155,400);
+		   fillcolorX(g, set_CG(color), 100,155,75,160,400);
+		   fillcolorX(g, set_CG(color), 75,160,50,165,400);
+		   fillcolorX(g, set_CG(color), 50,165,25,170,400);
+		   fillcolorX(g, set_CG(color), 25,170,0,175,400);
     }//method background_layer1_color1
-    private void background_layer1_color2(Graphics g){ //draw and fillcolor background_layer2
+     private void background_layer2_color2(Graphics g){ //draw and fillcolor background_layer2
 		int color =2; //background_layer1_color2
-		   fillcolorX(g, set_CG(color), 166,180,390,180,400);
-           fillcolorX(g, set_CG(color), 390,180,600,260,400);
-		   fillcolorX(g, set_CG(color), 165,180,-200,360,400);
+		   fillcolorX(g, set_CG(color), 275,160,300,160,400);
+           
+		   fillcolorX(g, set_CG(color), 300,160,325,165,400);
+		   fillcolorX(g, set_CG(color), 325,165,350,170,400);
+		   fillcolorX(g, set_CG(color), 350,170,375,175,400);
+		   fillcolorX(g, set_CG(color), 375,175,400,180,400);
+		   fillcolorX(g, set_CG(color), 400,180,425,185,400);
+		   fillcolorX(g, set_CG(color), 425,185,450,190,400);
+		   fillcolorX(g, set_CG(color), 450,190,475,195,400);
+		   fillcolorX(g, set_CG(color), 475,195,500,200,400);
+		   fillcolorX(g, set_CG(color), 500,200,525,205,400);
+		   fillcolorX(g, set_CG(color), 525,205,550,210,400);
+		   fillcolorX(g, set_CG(color), 550,210,575,215,400);
+		   fillcolorX(g, set_CG(color), 575,215,600,220,400);
 
+           fillcolorX(g, set_CG(color), 275,160,250,165,400);
+		   fillcolorX(g, set_CG(color), 250,165,225,170,400);
+		   fillcolorX(g, set_CG(color), 225,170,200,175,400);
+		   fillcolorX(g, set_CG(color), 200,175,175,180,400);
+		   fillcolorX(g, set_CG(color), 175,180,150,185,400);
+		   fillcolorX(g, set_CG(color), 150,185,125,190,400);
+		   fillcolorX(g, set_CG(color), 125,190,100,195,400);
+		   fillcolorX(g, set_CG(color), 100,195,75,200,400);
+		   fillcolorX(g, set_CG(color), 75,200,50,205,400);
+		   fillcolorX(g, set_CG(color), 50,205,25,210,400);
+		   fillcolorX(g, set_CG(color), 25,210,0,215,400);
     }//method background_layer1_color2
-	private void background_layer1_color3(Graphics g){ //draw and fillcolor background_layer3
+	private void background_layer3_color3(Graphics g){ //draw and fillcolor background_layer3
 		int color =3; //background_layer1_color3
-           fillcolorX(g, set_CG(color), 166,240,390,240,420);
-		   fillcolorX(g, set_CG(color), 390,240,600,320,420);
-		   fillcolorX(g, set_CG(color), 165,240,-200,420,420);
+           fillcolorX(g, set_CG(color), 275,200,300,200,420);
+
+		   fillcolorX(g, set_CG(color), 300,200,325,205,420);
+		   fillcolorX(g, set_CG(color), 325,205,350,210,420);
+		   fillcolorX(g, set_CG(color), 350,210,375,215,420);
+		   fillcolorX(g, set_CG(color), 375,215,400,220,420);
+		   fillcolorX(g, set_CG(color), 400,220,425,225,420);
+		   fillcolorX(g, set_CG(color), 425,225,450,230,420);
+		   fillcolorX(g, set_CG(color), 450,230,475,235,420);
+		   fillcolorX(g, set_CG(color), 475,235,500,240,420);
+		   fillcolorX(g, set_CG(color), 500,240,525,245,420);
+		   fillcolorX(g, set_CG(color), 525,245,550,250,420);
+		   fillcolorX(g, set_CG(color), 550,250,575,255,420);
+		   fillcolorX(g, set_CG(color), 575,255,600,260,420);
+
+		   fillcolorX(g, set_CG(color), 275,200,250,205,420);
+		   fillcolorX(g, set_CG(color), 250,205,225,210,420);
+		   fillcolorX(g, set_CG(color), 225,210,200,215,420);
+		   fillcolorX(g, set_CG(color), 200,215,175,220,420);
+		   fillcolorX(g, set_CG(color), 175,220,150,225,420);
+		   fillcolorX(g, set_CG(color), 150,225,125,230,420);
+		   fillcolorX(g, set_CG(color), 125,230,100,235,420);
+		   fillcolorX(g, set_CG(color), 100,235,75,240,420);
+		   fillcolorX(g, set_CG(color), 75,240,50,245,420);
+		   fillcolorX(g, set_CG(color), 50,245,25,250,420);
+		   fillcolorX(g, set_CG(color), 25,250,0,255,420);
     }//method background_layer1_color3
-	private void icefloor_layer1_color12_8(Graphics g){ //draw and fillcolor icefloor
-		int color1 =12; //icefloor_layer1_color12
-		int color2 =8;  //icefloor_layer1_color8
-		   fillcolorX(g, set_CG(color1), 0,420,600,420,600);
-		   fillcolorX(g, set_CG(color2), 0,440,600,440,600);
+	private void background_layer4_color12(Graphics g){ //draw and fillcolor icefloor
+		int color =12; //icefloor_layer1_color12
+		   fillcolorX(g, set_CG(color), 275,375,300,375,600);
+		   
+		   fillcolorX(g, set_CG(color), 300,375,325,380,600);
+		   fillcolorX(g, set_CG(color), 325,380,350,385,600);
+		   fillcolorX(g, set_CG(color), 350,385,375,390,600);
+		   fillcolorX(g, set_CG(color), 375,390,400,395,600);
+		   fillcolorX(g, set_CG(color), 400,395,425,400,600);
+		   fillcolorX(g, set_CG(color), 425,400,450,405,600);
+		   fillcolorX(g, set_CG(color), 450,405,475,410,600);
+		   fillcolorX(g, set_CG(color), 475,410,500,415,600);
+		   fillcolorX(g, set_CG(color), 500,415,525,420,600);
+		   fillcolorX(g, set_CG(color), 525,420,575,425,600);
+		   fillcolorX(g, set_CG(color), 575,425,600,430,600);
+		   
+		   fillcolorX(g, set_CG(color), 275,375,250,380,600);
+		   fillcolorX(g, set_CG(color), 250,380,225,385,600);
+		   fillcolorX(g, set_CG(color), 225,385,200,390,600);
+		   fillcolorX(g, set_CG(color), 200,390,175,395,600);
+		   fillcolorX(g, set_CG(color), 175,395,150,400,600);
+		   fillcolorX(g, set_CG(color), 150,400,125,405,600);
+		   fillcolorX(g, set_CG(color), 125,405,100,410,600);
+		   fillcolorX(g, set_CG(color), 100,410,75,415,600);
+		   fillcolorX(g, set_CG(color), 75,415,50,420,600);
+		   fillcolorX(g, set_CG(color), 50,420,25,425,600);
+		   fillcolorX(g, set_CG(color), 25,425,0,430,600);
     }//method icefloor_color12_8
+	private void background_layer5_color8(Graphics g){
+		int color =8;
+		   fillcolorX(g, set_CG(color), 275,395,300,395,600);
+
+		   fillcolorX(g, set_CG(color), 300,395,325,400,600);
+		   fillcolorX(g, set_CG(color), 325,400,350,405,600);
+		   fillcolorX(g, set_CG(color), 350,405,375,410,600);
+		   fillcolorX(g, set_CG(color), 375,410,400,415,600);
+		   fillcolorX(g, set_CG(color), 400,415,425,420,600);
+		   fillcolorX(g, set_CG(color), 425,420,450,425,600);
+		   fillcolorX(g, set_CG(color), 450,425,475,430,600);
+		   fillcolorX(g, set_CG(color), 475,430,500,435,600);
+		   fillcolorX(g, set_CG(color), 500,435,525,440,600);
+		   fillcolorX(g, set_CG(color), 525,440,550,445,600);
+		   fillcolorX(g, set_CG(color), 550,445,575,450,600);
+		   fillcolorX(g, set_CG(color), 575,450,600,455,600);
+		   
+		   fillcolorX(g, set_CG(color), 275,395,250,400,600);
+		   fillcolorX(g, set_CG(color), 250,400,225,405,600);
+		   fillcolorX(g, set_CG(color), 225,405,200,410,600);
+		   fillcolorX(g, set_CG(color), 200,410,175,415,600);
+		   fillcolorX(g, set_CG(color), 175,415,150,420,600);
+		   fillcolorX(g, set_CG(color), 150,420,125,425,600);
+		   fillcolorX(g, set_CG(color), 125,425,100,430,600);
+		   fillcolorX(g, set_CG(color), 100,430,75,435,600);
+		   fillcolorX(g, set_CG(color), 75,435,50,440,600);
+		   fillcolorX(g, set_CG(color), 50,440,25,445,600);
+		   fillcolorX(g, set_CG(color), 25,445,0,450,600);
+
+    }
+
 	private void icepole_color9(Graphics g){ //draw and fillcolor icepole
 		int color =9;// icepole_color9
 		fillcolorX(g, set_CG(color), 130,120,165,120,420);
